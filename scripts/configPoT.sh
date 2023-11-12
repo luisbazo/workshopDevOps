aff776938ed81eecf675cc3642b4eaab61a8a7c2
Luis Bazo Garcia <luis.bazo@gmail.com>
2023-11-13 00:14:08 +0100

    update configpot to new devops materials

--- scripts/configPoT.sh
+++ scripts/configPoT.sh
@@ -72,15 +72,15 @@ while read line; do
 #   echo "${pass}"
 
    #create the user in the linux system and add it to the docker group
-   adduser user$i 
+   adduser user$i
 
    echo "$line   user$i:$pass"
    echo "user$i:$pass" | chpasswd
    usermod -a -G docker user$i
    usermod --add-subuids 200000-201000 --add-subgids 200000-201000 user$i
-   
-   passwords="$passwords user$i=$pass"   
-   
+
+   passwords="$passwords user$i=$pass"
+
    i=$(($i+1))
 #  echo $i
 done < $filename
@@ -94,7 +94,7 @@ while read line; do
    #create the namespace
    echo "oc new-project ns$i"
    oc new-project ns$i
-   #give rights to each user to its project 
+   #give rights to each user to its project
    echo "   oc -n ns$i patch rolebinding admin --type='json' -p='[{"op": "replace", "path": "/subjects/0", "value":{"apiGroup": "rbac.authorization.k8s.io","kind": "User","name": "IAM#$line"}}]'  "
    oc -n ns$i patch rolebinding admin --type='json' -p='[{"op": "replace", "path": "/subjects/0", "value":{"apiGroup": "rbac.authorization.k8s.io","kind": "User","name": "IAM#$line"}}]'
 
@@ -109,29 +109,29 @@ while read line; do
 
    # needed to give root rights to docker images
    oc adm policy add-scc-to-user anyuid -z default -n ns$i
-   
+
    # need cluste role self-provisioner to create project for ci cd
    oc adm policy add-cluster-role-to-user self-provisioner IAM#$line
- 
+
    #copy some yaml files used during the labs to the new user home folder
    cp -r ./artifacts /home/user$i
    chown -R user$i:user$i /home/user$i/artifacts
-   
+
    ls /home/user$i/artifacts/*.yaml | awk '{print $1}' | xargs sed -i "s/userX/user$i/g"
    ls /home/user$i/artifacts/*.yaml | awk '{print $1}' | xargs sed -i "s/nsX/ns$i/g"
-   
-   
+
+
    cp ./artifacts/javaee/HelloK8s.war /home/user$i
    chown user$i:user$i /home/user$i/HelloK8s.war
-   
-   for e in dev qa staging production; do
+
+   for e in dev sta prod; do
    # create a new namespace for each user and env
       # make user admin of the new project
-      oc policy add-role-to-user admin IAM#${line} -n project${i}-${e} 
+      oc policy add-role-to-user admin IAM#${line} -n ${e}
    done
-   
-   #Add users to the admin groups of devtoolkit needed  
-   for group in ibm-toolkit-users argocd-admins; do
+
+   #Add users to the admin groups of devtoolkit needed
+   for group in argocd-admins; do
       oc adm groups add-users ${group} IAM#${line}
    done
 
